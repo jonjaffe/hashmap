c010cae432e88cf6ea158a82c49e42dde0229d03
App Academy Student <student@appacademy.io>
2017-04-07 14:40:16 -0700

    Complete p02 hashing functions. really cool.

--- skeleton/lib/p02_hashing.rb
+++ skeleton/lib/p02_hashing.rb
@@ -2,13 +2,19 @@ class Fixnum
   # Fixnum#hash already implemented for you
 end
 
+class Symbol
+
+end
+
 class Array
   def hash
+    self.join("").to_i.hash
   end
 end
 
 class String
   def hash
+    self.chars.map { |ch| ch.ord }.join("").to_i.hash
   end
 end
 
@@ -16,6 +22,28 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+
+    garbage_sum = 0
+
+    self.to_a.flatten.each do |el|
+      if el.is_a? Symbol
+        garbage_sum += el.to_s.hash
+      elsif el.is_a? Integer 
+        garbage_sum += (el + 97).chr.hash
+      else
+        garbage_sum += el.hash
+      end
+    end
+
+    garbage_sum
   end
 end
+
+
+# storage = Array.new(5) { [] }
+#
+# self.each do |key, val|
+#   storage[key.hash % storage.count] << [key, val]
+# end
+#
+# storage
