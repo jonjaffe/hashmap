c8e2840c418cf5d53e44690f869bca3101934e16
App Academy Student <student@appacademy.io>
2017-04-07 14:55:34 -0700

    Complete p03 with full functionality

--- skeleton/lib/p03_hash_set.rb
+++ skeleton/lib/p03_hash_set.rb
@@ -1,7 +1,7 @@
 require_relative 'p02_hashing'
 
 class HashSet
-  attr_reader :count
+  attr_reader :count, :store
 
   def initialize(num_buckets = 8)
     @store = Array.new(num_buckets) { Array.new }
@@ -9,18 +9,24 @@ class HashSet
   end
 
   def insert(key)
+    @count += 1
+    resize! if @count > num_buckets
+    self[key] << key
   end
 
   def include?(key)
+    self[key].any? {|n| n == key}
   end
 
   def remove(key)
+    self[key].reject! {|n| n == key}
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num.hash % num_buckets]
   end
 
   def num_buckets
@@ -28,5 +34,11 @@ class HashSet
   end
 
   def resize!
+    old_elements = @store.flatten
+    new_arr = HashSet.new(num_buckets)
+    old_elements.each do |el|
+      new_arr.insert(el)
+    end
+    @store = new_arr.store
   end
 end
